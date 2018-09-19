3eba51f296da722fda21c2db94de331c76f8f17d
Pete Moore <pete@lookatyourdata.com>
2018-09-19 12:30:29 +0100

    20180919123029

--- Python/setup.py
+++ Python/setup.py
@@ -5,19 +5,28 @@ Created on Wed Sep 19 11:28:58 2018
 
 @author: petermoore
 """
+PATH_HOME="/Users/petermoore/Documents/GitHub/VacationVision/Python"
+import os
+os.chdir(PATH_HOME)
+print(os.getcwd())
 PATH_CONNYAML = "connections.yaml" #NB this file ignored from GitHub
-def getDSNfromYAML(yamlfile):
+
+# function to read yaml file and get connection strings (0=SQL, 1=MONGO)
+def getDSNfromYAML(yamlfile, yamlindex):
     import yaml
     with open(yamlfile, 'r') as f:
         doc = yaml.load(f)
-        pdsn = doc["sqlconn"]["DSN"]
-        puser = doc["sqlconn"]["user"]
-        ppassword = doc["sqlconn"]["password"]
+        pdsn = doc[yamlindex]["DSN"]
+        puser = doc[yamlindex]["user"]
+        ppassword = doc[yamlindex]["password"]
+        pport = doc[yamlindex]["port"]
     dsn="DSN="+pdsn+";UID="+puser+";PWD="+ppassword
     alchemydsn = "mssql+pyodbc://"+puser+":"+ppassword+"@"+pdsn
-    return dsn, alchemydsn, pdsn, puser, ppassword
+    return dsn, alchemydsn, pdsn, puser, ppassword, pport
 
 
 
-import os
-print(os.getpwd())
+
+sqldsn, sqlalchemydsn, sqldsn, sqluser, sqlpassword, _ = getDSNfromYAML(PATH_CONNYAML, 0)
+_, _, _, mongouser, mongopassword, mongoport = getDSNfromYAML(PATH_CONNYAML, 1)
+
